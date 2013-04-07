e8ae18a2703c0ff331b877c28cb3b86e0af6b590
Sebastian Gaul <sebastian@mgvmedia.com>
2013-04-07 15:34:25 +0200

    Add tasks to start and stop web servers

--- README.md
+++ README.md
@@ -2,7 +2,7 @@ node-build-script
 =================
 
 A simple build script for Coffeescript-based Node applications. It supports file
-change monitoring to trigger real-time deployment and test execution.
+change monitoring to trigger real-time deployment, test execution and web server (re-)starting.
 
 Usage
 -----
@@ -12,12 +12,22 @@ Usage
 Tasks
 -----
 
-      build:       Build project.
-      test:        Build project and build and runs all tests.
-                   Equivalent to "build.sh build build-tests run-tests".
-      watch:       Start infinite loop triggering succeeding tasks whenever a file changes in
-                   project directory. Use only once.
-      dev:         Build everything, runs tests and repeats whenever a file changes.
-                   Equivalent to "build.sh test watch test".
-      build-tests: Build all tests.
-      run-tests:   Run all tests.
+      build:          Build project.
+
+      test:           Build project and build and runs all tests.
+                      Equivalent to "build.sh build build-tests run-tests".
+      build-tests:    Build all tests.
+      run-tests:      Run all tests.
+
+      server:         Stop running server (if existing), build project and start server.
+                      Equivalent to "build.sh stop-server build start-server".
+      restart-server: Stop running server (if existing) and start server.
+                      Equivalent to "build.sh stop-server start-server".
+      start-server:   Start server.
+      stop-server:    Stop running server (if existing).
+
+      watch:          Start infinite loop triggering succeeding tasks whenever a file changes in
+                      project directory. Use only once.
+      dev:            Build everything, runs tests and repeats whenever a file changes.
+                      Equivalent to "build.sh test watch test".
+
--- build.sh
+++ build.sh
@@ -12,15 +12,24 @@ if [[ "$#" == "0" ]]; then
     echo "Usage: $name [ task1 ] [ task2 ] [ task3 ] [ ... ]"
     echo
     echo "Tasks:"
-    echo "  build:       Build project."
-    echo "  test:        Build project and build and runs all tests."
-    echo "               Equivalent to \"$name build build-tests run-tests\"."
-    echo "  watch:       Start infinite loop triggering succeeding tasks whenever a file changes in"
-    echo "               project directory. Use only once."
-    echo "  dev:         Build everything, runs tests and repeats whenever a file changes."
-    echo "               Equivalent to \"$name test watch test\"."
-    echo "  build-tests: Build all tests."
-    echo "  run-tests:   Run all tests."
+    echo "  build:          Build project."
+    echo
+    echo "  test:           Build project and build and runs all tests."
+    echo "                  Equivalent to \"$name build build-tests run-tests\"."
+    echo "  build-tests:    Build all tests."
+    echo "  run-tests:      Run all tests."
+    echo
+    echo "  server:         Stop running server (if existing), build project and start server."
+    echo "                  Equivalent to \"$name stop-server build start-server\"."
+    echo "  restart-server: Stop running server (if existing) and start server."
+    echo "                  Equivalent to \"$name stop-server start-server\"."
+    echo "  start-server:   Start server."
+    echo "  stop-server:    Stop running server (if existing)."
+    echo
+    echo "  watch:          Start infinite loop triggering succeeding tasks whenever a file changes in"
+    echo "                  project directory. Use only once."
+    echo "  dev:            Build everything, runs tests and repeats whenever a file changes."
+    echo "                  Equivalent to \"$name test watch test\"."
     exit 1
 fi
 
@@ -84,6 +93,22 @@ run_tests() {
     fi
 }
 
+server_pid=0
+
+start_server() {
+    echo "Start server..."
+    $application_launcher "$build_directory/run-server.js" &
+    server_pid=$!
+}
+
+stop_server() {
+    if [[ $server_pid > 0 ]]; then
+        echo "Stop server..."
+        kill $server_pid
+        server_pid=0
+    fi
+}
+
 
 watch() {
     echo "Waiting for file changes..."
@@ -112,12 +137,30 @@ process_tasks() {
             'run-tests')
                 run_tests
                 ;;
+            'server')
+                stop_server
+                build_project
+                start_server
+            'restart-server')
+                stop_server
+                start_server
+                ;;
+            'start-server')
+                start_server
+                ;;
+            'stop-server')
+                stop_server
+                ;;
             'watch')
                 watch
                 ;;
             'dev')
                 ./$0 test watch test
                 exit
+                ;;
+            *)
+                echo "WARNING: Unknown task $task. Skip and continue..."
+                ;;
         esac
     done
 }
